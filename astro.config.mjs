4bf60e179d7b1d00bd1249c4eeffb8ecfd40640c
Matt Davis <hello.mattdavis@gmail.com>
2022-10-31 21:13:55 -0400

    lottie file placement/ project expansion

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,11 @@ import svelte from '@astrojs/svelte';
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [svelte()],
+  integrations: [svelte(), mdx()],
   adapter: netlify()
 });
