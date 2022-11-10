6dd79cc9c59f7a571dc516050034313b278d8b56
Matt Davis <hello.mattdavis@gmail.com>
2022-11-09 22:41:09 -0500

    netlify??

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,6 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   integrations: [svelte(), mdx()],
-  adapter: 'netlify',
+  adapter: netlify(),
 });
