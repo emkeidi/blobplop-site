967fca87b850e386d409a650a3d0a1d27acbc8ed
Matt Davis <hello.mattdavis@gmail.com>
2022-11-10 15:05:06 -0500

    static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import svelte from '@astrojs/svelte';
 
-// https://astro.build/config
-import netlify from '@astrojs/netlify/functions';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
-  output: 'server',
-  adapter: netlify()
+  integrations: [svelte()]
 });
