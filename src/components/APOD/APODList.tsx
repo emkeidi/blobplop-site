f04d572cd52e84992b2b6b3e8cbfc00725ec02c3
Matt Davis <hello.mattdavis@gmail.com>
2023-07-02 12:15:23 -0400

    removed bootstrap, nasa images need more work

--- src/components/APOD/APODList.tsx
+++ src/components/APOD/APODList.tsx
@@ -1,66 +1,66 @@
 import { useEffect, useState } from 'react';
-import SpaceImagesModel from '../../../models/SpaceImagesModel';
+import SpaceImagesModel from '../../models/SpaceImagesModel';
 import SpaceImageCard from './SpaceImageCard';
 
 export const APODList = () => {
-  const [httpError, setHttpError] = useState(null);
+	const [httpError, setHttpError] = useState(null);
 
-  const [images, setImages] = useState<SpaceImagesModel[]>([]);
-  const [isLoadingImages, setIsLoadingImages] = useState(true);
-  const [newImages, setNewImages] = useState(false);
+	const [images, setImages] = useState<SpaceImagesModel[]>([]);
+	const [isLoadingImages, setIsLoadingImages] = useState(true);
+	const [newImages, setNewImages] = useState(false);
 
-  useEffect(() => {
-    const fetchSpaceImages = async () => {
-      const imagesResponse = await fetch('https://api.blobplop.com/api/v1/apod');
+	useEffect(() => {
+		const fetchSpaceImages = async () => {
+			const imagesResponse = await fetch('https://api.blobplop.com/api/v1/apod');
 
-      if (!imagesResponse.ok) {
-        throw new Error('something went wrong in space!');
-      }
-      const imagesResponseJson = await imagesResponse.json();
-      setImages(imagesResponseJson);
-      setIsLoadingImages(false);
-    };
-    fetchSpaceImages().catch((error) => {
-      setIsLoadingImages(false);
-      setHttpError(error.message);
-    });
-  }, [newImages]);
+			if (!imagesResponse.ok) {
+				throw new Error('something went wrong in space!');
+			}
+			const imagesResponseJson = await imagesResponse.json();
+			setImages(imagesResponseJson);
+			setIsLoadingImages(false);
+		};
+		fetchSpaceImages().catch((error) => {
+			setIsLoadingImages(false);
+			setHttpError(error.message);
+		});
+	}, [newImages]);
 
-  if (httpError) {
-    return (
-      <div>
-        <p>{httpError}</p>
-      </div>
-    );
-  }
+	if (httpError) {
+		return (
+			<div>
+				<p>{httpError}</p>
+			</div>
+		);
+	}
 
-  for (let image of images) {
-    if (image.copyright == null) {
-      image.copyright = '';
-    } else {
-      image.copyright = `Credit: ${image.copyright}`;
-    }
-  }
+	for (let image of images) {
+		if (image.copyright == null) {
+			image.copyright = '';
+		} else {
+			image.copyright = `Credit: ${image.copyright}`;
+		}
+	}
 
-  return (
-    <div className='space-images-list'>
-      <section className='space-images container-fluid' id='space-images'>
-        <div className='col text-center mb-3'>
-          <button className='btn btn-primary m-2 p-2' onClick={() => setNewImages(!newImages)}>
-            New Images
-          </button>
-        </div>
-        <div className='row row-cols-1'>
-          {images.map((image) => (
-            <SpaceImageCard key={image.url} image={image} />
-          ))}
-        </div>
-        <div className='col text-center'>
-          <button className='btn btn-primary mb-4 p-2' onClick={() => setNewImages(!newImages)}>
-            New Images
-          </button>
-        </div>
-      </section>
-    </div>
-  );
+	return (
+		<div className='space-images-list'>
+			<section className='space-images container-fluid' id='space-images'>
+				<div className='col text-center mb-3'>
+					<button className='btn btn-primary m-2 p-2' onClick={() => setNewImages(!newImages)}>
+						New Images
+					</button>
+				</div>
+				<div className='row row-cols-1'>
+					{images.map((image) => (
+						<SpaceImageCard key={image.url} image={image} />
+					))}
+				</div>
+				<div className='col text-center'>
+					<button className='btn btn-primary mb-4 p-2' onClick={() => setNewImages(!newImages)}>
+						New Images
+					</button>
+				</div>
+			</section>
+		</div>
+	);
 };
--- src/components/APOD/SpaceImageCard.tsx
+++ src/components/APOD/SpaceImageCard.tsx
@@ -1,50 +1,56 @@
-import SpaceImagesModel from '../../../models/SpaceImagesModel';
+import SpaceImagesModel from '../../models/SpaceImagesModel';
 import './SpaceImageCard.css';
 
 interface SpaceImageCardProps {
-  image: SpaceImagesModel;
+	image: SpaceImagesModel;
 }
 
 export default function SpaceImageCard({ image }: SpaceImageCardProps) {
-  if (image.media_type === 'image') {
-    return (
-      <div className='col'>
-        <div className='card shadow text-bg-dark mb-3'>
-          <img src={image.url} alt={image.title} className='card-img-top' />
-          <div className='card-body'>
-            <h5 className='card-title'>{image.title}</h5>
-            <h6 className='card-subtitle mb-2'>
-              {image.date} {image.copyright}
-            </h6>
-            <p className='card-text'>{image.explanation}</p>
-            <p>
-              <a href={image.hdurl} target='_blank'>
-                Link to original
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className='col'>
-        <div className='card shadow text-bg-dark mb-3 g-3'>
-          <iframe src={image.url} allow='autoplay' />
-          <div className='card-body'>
-            <h5 className='card-title'>{image.title}</h5>
-            <h6 className='card-subtitle mb-2'>
-              {image.date} {image.copyright}
-            </h6>
-            <p className='card-text'>{image.explanation}</p>
-            <p>
-              <a href={image.url} target='_blank'>
-                Link to original
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	if (image.media_type === 'image') {
+		return (
+			<div className='col'>
+				<div className='card shadow text-bg-dark mb-3'>
+					<img src={image.url} alt={image.title} className='card-img-top' />
+					<div className='card-body'>
+						<details>
+							<summary>
+								<em>{image.title}</em>, {image.date}
+								{image.copyright && <p>{image.copyright}</p>}
+							</summary>
+							<p className='card-text'>{image.explanation}</p>
+						</details>
+						<p>
+							<a href={image.hdurl} target='_blank' rel='noopener'>
+								Link to original
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className='col'>
+				<div className='card shadow text-bg-dark mb-3 g-3'>
+					<iframe src={image.url} allow='autoplay' />
+					<div className='card-body'>
+						<details>
+							<summary>
+								<h5 className='card-title'>{image.title}</h5>
+								<h6 className='card-subtitle mb-2'>
+									{image.date} {image.copyright}
+								</h6>
+							</summary>
+							<p className='card-text'>{image.explanation}</p>
+						</details>
+						<p>
+							<a href={image.url} target='_blank' rel='noopener'>
+								Link to original
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
