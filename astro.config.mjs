54479699aacbd5640f01be492a82fcf5b68a768a
Matt Davis <hello.mattdavis@gmail.com>
2023-07-02 12:20:36 -0400

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 import svelte from '@astrojs/svelte';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
 	output: 'hybrid',
+	adapter: netlify(),
 	integrations: [svelte(), react(), tailwind()],
 	markdown: {
 		smartypants: false,
