b4dc780a4a604eec316e45bd80a4b2af476633b3
Matt Davis <hello.mattdavis@gmail.com>
2023-07-29 13:18:42 -0400

    themes, postcss, svg icon support

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,15 +2,35 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		container: {
+			center: true,
+		},
 		fontFamily: {
 			sans: ['sans-serif'],
 			serif: ['Lora', 'serif'],
 		},
 		extend: {},
 	},
-	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	daisyui: {
-		themes: ['light', 'dark'],
+		themes: [
+			{
+				blobby: {
+					primary: '#7ee7abff',
+					secondary: '#3aba94ff',
+					accent: '#d4b966ff',
+					neutral: '#3d312bff',
+					'base-100': '#0a0808ff',
+					info: '#9c5c2fff',
+					success: '#9b4da8ff',
+					warning: '#fbbd23',
+					error: '#f87272',
+				},
+			},
+			'retro',
+			'coffee',
+			'emerald',
+		],
 	},
 	darkmode: 'class',
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
