21c8b3083f3e0171b80b5c07da6b8ef71e87892e
Matt Davis <hello.mattdavis@gmail.com>
2023-10-13 18:50:32 -0400

    astro update and removed theme-change. will implement another way

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,26 +11,5 @@ module.exports = {
 		},
 		extend: {},
 	},
-	daisyui: {
-		themes: [
-			{
-				blobby: {
-					primary: '#7ee7abff',
-					secondary: '#f0abfc',
-					accent: '#d4b966ff',
-					neutral: '#57534e',
-					'base-100': '#1f2937',
-					info: '#9c5c2fff',
-					success: '#9b4da8ff',
-					warning: '#fbbd23',
-					error: '#f87272',
-				},
-			},
-			'retro',
-			'coffee',
-			'fantasy',
-		],
-	},
-	darkmode: 'class',
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
