14bdffa309416e500f97166216a78cecae1a01b2
Matt Davis <hello.mattdavis@gmail.com>
2025-01-18 23:41:08 -0500

    test title css

--- src/components/RandomTagline.tsx
+++ src/components/RandomTagline.tsx
@@ -1,34 +1,48 @@
-import React from 'react';
-import './RandomTagline.css';
+import React from "react";
+import "./RandomTagline.css";
 
 type RandomTaglineProps = {
 	word?: string;
+	extraWord?: string;
 };
 
-const RandomTagline: React.FC<RandomTaglineProps> = ({ word = 'Blob' }) => {
+const RandomTagline: React.FC<RandomTaglineProps> = ({ word = "Blob", extraWord = "Blobbington" }) => {
 	// Array of random taglines
 	const taglines = [
-		'All roads lead to',
-		'Unlock the power of',
-		'Embrace the magic of',
-		'Discover the wonders of',
-		'Experience the joy of',
-		'Unleash your creativity with',
-		'Find your inspiration in',
-		'Navigate the chaos with',
-		'Embrace the adventure with',
-		'Transform your world with',
+		"All roads lead to ${word}",
+		"Unlock the power of ${word}",
+		"Embrace the magic of ${word}",
+		"Discover the wonders of ${word}",
+		"Experience the joy of ${word}",
+		"Unleash your creativity with ${word}",
+		"Find your inspiration in ${word}",
+		"Navigate the chaos with ${word}",
+		"Embrace the adventure with ${word}",
+		"Transform your world with ${word}",
+		"${word} is the key to success",
+		"${word} is the answer",
+		"${word} is the way",
+		"${word} ${word} ${word}",
+		"${word} ${extraWord} called me last night, do you know what he wanted?",
+		"Moving to ${extraWord} was the best decision I ever made",
 	];
 
-	// Get a random tagline
-	const randomTagline = taglines[Math.floor(Math.random() * taglines.length)];
+	// Function to get a random text gradient class
+	const getRandomGradientClass = () => {
+		const classes = ["text-gradient hover:blur-sm", "text-gradient-2 hover:blur-sm", "text-gradient-3 hover:blur-sm"];
+		return classes[Math.floor(Math.random() * classes.length)];
+	};
 
-	return (
-		<h1 className='text-4xl font-bold'>
-			<span className=''>{randomTagline} </span>
-			<span className='text-gradient'>{word}</span>
-		</h1>
-	);
+	// Get a random tagline
+	let randomTagline = taglines[Math.floor(Math.random() * taglines.length)];
+
+	// Replace all instances of ${word} and ${extraWord} with spans containing the word and random gradient class
+	const wordSpan = `<span class="${getRandomGradientClass()}">${word}</span>`;
+	const extraWordSpan = `<span class="${getRandomGradientClass()}">${extraWord}</span>`;
+	randomTagline = randomTagline.replace(/\${word}/g, wordSpan);
+	randomTagline = randomTagline.replace(/\${extraWord}/g, extraWordSpan);
+
+	return <h1 className="text-4xl font-bold" dangerouslySetInnerHTML={{ __html: randomTagline }} />;
 };
 
 export default RandomTagline;
